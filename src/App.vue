eb8200bf764e0d68a27a7043a06ae28b8a52273a
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-03-11 15:14:06 +0100

    add: responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <div id="app" class="min-h-screen bg-gray-100">
-    <div class="px-8 py-12">
-      <Logo class="h-10 mb-6" />
+  <div class="bg-gray-100 lg:grid lg:grid-cols-2 2xl:grid-cols-5">
+    <div
+      class="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 xl:max-w-xl 2xl:col-span-2"
+    >
+      <Logo class="h-10 mb-6 sm:mb-8" />
       <Landing />
+    </div>
+    <div class="hidden relative lg:block 2xl:col-span-3">
+      <img
+        class="absolute inset-0 w-full h-full object-cover object-center"
+        src="./assets/img/beach-work.jpg"
+        alt="Woman working on the beach"
+      />
     </div>
   </div>
 </template>
@@ -18,4 +27,8 @@ export default defineComponent({
 })
 </script>
 
-<style></style>
+<style>
+body {
+  @apply bg-gray-100;
+}
+</style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,19 +1,21 @@
 <template>
   <img
-    class="rounded-lg shadow-xl mb-6"
+    class="rounded-lg shadow-xl mb-6 sm:mb-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
     src="../assets/img/beach-work.jpg"
     alt="Woman working on the beach"
   />
-  <h1 class="text-2xl font-bold text-gray-800 mb-2">
+  <h1
+    class="text-2xl font-bold text-gray-800 mb-2 sm:text-4xl sm:mb-4 lg:text-3xl xl:text-4xl"
+  >
     You can work from anywhere.
-    <span class="text-indigo-500">Take advantage of it.</span>
+    <p class="text-indigo-500">Take advantage of it.</p>
   </h1>
-  <p class="text-gray-600 mb-4">
+  <p class="text-gray-600 mb-4 sm:text-xl sm:mb-6">
     Workcation helps you find work-friendly rentals in beautiful locations so
     you can enjoy some nice weather even when you're not on vacation.
   </p>
   <a
-    class="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-white uppercase tracking-wider font-semibold text-sm"
+    class="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-white uppercase tracking-wider font-semibold text-sm sm:text-base"
     href="#"
   >
     Book your escape
