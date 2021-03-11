6313665f73fd4310d76012975ff3eecd6f1f85d8
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-03-11 14:23:22 +0100

    add: vite-svg-loader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" class="min-h-screen bg-gray-100">
+    <logo />
     <landing />
   </div>
 </template>
@@ -7,10 +8,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Landing from './components/Landing.vue'
+import Logo from './components/shared/Logo.vue'
 
 export default defineComponent({
   name: 'App',
-  components: { Landing }
+  components: { Landing, Logo }
 })
 </script>
 
---
+++ src/components/shared/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <LogoSVG />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LogoSVG from '../../assets/img/logo.svg'
+
+export default defineComponent({
+  name: 'Logo',
+  components: { LogoSVG }
+})
+</script>
+
+<style></style>
