32d1307839bd304f2f30ebd12ad6c812ae6d5eab
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-03-11 13:49:01 +0100

    add: tailwindCSS purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 module.exports = {
-  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
