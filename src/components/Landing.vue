d4f3d6613b1e7965e54d9ca35c35c3b78954eb04
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-03-11 14:51:13 +0100

    add: start landing

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,9 +1,23 @@
 <template>
   <img
-    class="rouded"
+    class="rounded-lg shadow-xl mb-6"
     src="../assets/img/beach-work.jpg"
     alt="Woman working on the beach"
   />
+  <h1 class="text-2xl font-bold text-gray-800 mb-2">
+    You can work from anywhere.
+    <span class="text-indigo-500">Take advantage of it.</span>
+  </h1>
+  <p class="text-gray-600 mb-4">
+    Workcation helps you find work-friendly rentals in beautiful locations so
+    you can enjoy some nice weather even when you're not on vacation.
+  </p>
+  <a
+    class="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-white uppercase tracking-wider font-semibold text-sm"
+    href="#"
+  >
+    Book your escape
+  </a>
 </template>
 
 <script lang="ts">
